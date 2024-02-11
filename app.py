394c3146794a1af870edeb54d172d56d22a587da
Raedalfa <159704671+Raedalfa@users.noreply.github.com>
2024-02-11 22:47:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 # projectalfa
-git add requirements.txt
-git commit -m "Add requirements.txt with necessary packages"
-git push origin main
 import streamlit as st
 from metapub import PubMedFetcher
 
