ab40650d0ea5c17565739a72f3699ed2d3f21021
Raedalfa <159704671+Raedalfa@users.noreply.github.com>
2024-02-11 18:00:35 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,5 @@
 # projectalfa
-pip install streamlit
 pip install requests
 import streamlit as st
 st.title('Revolutionizing Systematic Reviews in Medicine')
+query = st.text_input("Enter your search query:")
